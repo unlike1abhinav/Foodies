77ae4fd993e915c7ef929ee72c0fb1d8c49c888e
unlike1abhinav <dhimanabhinav8295@gmail.com>
2025-03-02 04:30:14 +0530

    main-header

---
+++ components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
