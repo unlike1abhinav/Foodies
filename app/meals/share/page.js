0c04d88ac5135d4004e0f097b7758429e50774cd
unlike1abhinav <dhimanabhinav8295@gmail.com>
2025-03-05 02:06:03 +0530

    meal-share

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,10 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+import MealFormSubmitter from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +42,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker/>
+          <ImagePicker label='yOUR IMAGE' name='image'/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealFormSubmitter/>
           </p>
         </form>
       </main>
